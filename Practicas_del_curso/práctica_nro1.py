1c51cbc0056894891b2ce485aee36d485cfdca0a
marianavalencia-roj <mvalenciaroj@unsa.edu.pe>
2025-01-21 03:21:18 +0000

    prueba de subir

---
+++ Practicas_del_curso/práctica_nro1.py
@@ -0,0 +1,20 @@
+#enunciado nro4
+print("Hola mundo")
+#enunciado nro5 
+nombre=input("Ingrese su nombre")
+print("Hola "+nombre)
+#enunciado nro6
+edad=int(input("Ingresa tu edad"))
+if edad>=18:
+    print("Usted es mayor de edad")
+else: 
+    print("Usted es menor de edad")
+#enunciado nr7
+nro=input("Ingrese un numero")
+if nro%2==0:
+   print(nro,"es par")
+else: 
+    print("El número "+ nro +" es impar")
+#enunciado nro8
+nro1=input("Ingrese un # entero ")
+nro2=1+1
